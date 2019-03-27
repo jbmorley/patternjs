bcd1ec47a3394887bf699f0c94f96d7e747dcc11
Jason Barrie Morley <jason.morley@inseven.co.uk>
2019-03-27 07:08:28 -0700

    Initial work to set a download filename.

--- utilities.js
+++ utilities.js
@@ -4,22 +4,40 @@ var Utilities = {
         document.addEventListener('DOMContentLoaded', callback, false);
     },
 
-    downloadButton: function(width, height, title, renderer) {
+    downloadButton: function(name, width, height, title, renderer) {
         var button = document.createElement('div');
         button.innerHTML = title;
         button.classList.add('download');
         button.addEventListener('click', function() {
-            Utilities.download(width, height, renderer);
+            Utilities.download(name, width, height, renderer);
         });
         return button;
     },
 
-    download: function(width, height, renderer) {
+    download: function(name, width, height, renderer) {
         var canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
         renderer(canvas);
-        window.open(canvas.toDataURL('image/png'));
+        var a = document.createElement('a');
+        a.href = canvas.toDataURL('image/png');
+        a.download = name + '_' + width + 'x' + height + '.png';
+        if (typeof MouseEvent === "function") {
+            event = new MouseEvent("click", {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+                ctrlKey: false,
+                altKey: false,
+                shiftKey: false,
+                metaKey: false,
+                button: 0,
+                buttons: 1,
+            });
+            a.dispatchEvent(event);
+        } else if (a.fireEvent) {
+            a.fireEvent("onclick");
+        }
     },
 
 }
